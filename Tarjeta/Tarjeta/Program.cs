1a981f33535aeedcfcf252f831e99fb00f6473f6
LuisEd0105 <20198315@ilta.edu.do>
2020-06-02 21:30:13 -0400

    programas 1er pracial

---
+++ Tarjeta/Tarjeta/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarjeta_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double precio;
+            string metpago;
+            double numtarjeta;
+
+            Console.Write("Escriba el precio del producto: ");
+            precio = double.Parse(Console.ReadLine());
+
+            Console.Write("Escriba el metodo de pago: ");
+            metpago = (Console.ReadLine());
+
+            if (metpago.Equals("tarjeta"))
+            {
+                Console.Write("Digite el numero de la tarjeta: ");
+                numtarjeta = double.Parse(Console.ReadLine());
+                
+                Console.WriteLine("El producto con precio " + precio + " se ha pagado" + 
+                " con el numero cuenta " + numtarjeta);
+            }
+
+            else if (metpago.Equals("efectivo"))
+            {
+                Console.WriteLine("El producto con precio " + precio + " se ha pagado correctamente!");
+            }
+
+            else
+            {
+                Console.WriteLine("El metodo de pago no es valido");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ejercicio 1 parcial/ejercicio 1 parcial/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_1_parcial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Escribe un dia de la semana: ");
+                String dia = Console.ReadLine();
+
+                switch (dia)
+                {
+                    case "lunes":
+                    case "martes":
+                    case "miercoles":
+                    case "jueves":
+                    case "viernes":
+
+                        Console.WriteLine("No es fin de semana");
+                        break;
+
+                    case "sabado":
+                    case "domingo":
+
+                        Console.WriteLine("Es fin de semana");
+                        break;
+                }
+            }
+            Console.WriteLine("\nPresione enter para finalizar");
+            Console.ReadKey();
+        }
+        }
+    }
